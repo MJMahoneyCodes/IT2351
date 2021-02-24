68df3bff9db69aa5cdd97df8a4b7001ab8d14ab4
MJMahoneyCodes <ziggurattt@gmail.com>
2021-02-23 19:50:11 -0500

    Add files via upload

--- Assignment2/Assignment2Part2D.sql
+++ Assignment2/Assignment2Part2D.sql
@@ -1,13 +1,10 @@
 -- IT 2351 Spring 2021 Assignment 2 Part 2D Matt Mahoney
--- This joins the customers and orders and order_details tables
--- The primary key of the customers table is customer_id joined with foreign key (same name) of orders table
--- The primary key of the orders table is order_id joined with foreign key (same name) of the order_details table
--- The primary key of the items table is item_id joined with foreign key (same name) of the order_details table
+-- The primary key of the orders table is order_id joined with foreign key (same name) of order_details
+-- The primary key of the order_details table is the item_id joined with foreign key (same name) of items
 
-SELECT customers.customer_id, order_date, artist, order_qty
-FROM customers JOIN orders
-ON customers.customer_id = orders.customer_id
-JOIN order_details
+SELECT customer_id, order_date, artist, order_qty
+FROM orders JOIN order_details
 ON orders.order_id = order_details.order_id
-JOIN items ON items.item_id = order_details.item_id
+JOIN items
+ON order_details.item_id = items.item_id
 ORDER BY customer_id;
