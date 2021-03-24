bb9c4abae06ed7c2c8e57fd9c3b41c2cb1e26e2f
MJMahoneyCodes <ziggurattt@gmail.com>
2021-03-24 08:49:34 -0400

    Delete Assignment2 directory

--- Assignment2/Assignment2Part2A.sql
+++
@@ -1,8 +0,0 @@
--- IT 2351 Spring 2021 Assignment 2 Part 2A Matt Mahoney
--- This joins the items and order_details tables
--- The primary key of the items table is item_id
--- The foreign key in the order_details table is item_id
-SELECT title, artist, unit_price, order_qty
-FROM items JOIN order_details
-ON items.item_id = order_details.item_id
-ORDER BY title, artist;
--- Assignment2/Assignment2Part2H.sql
+++
@@ -1,7 +0,0 @@
--- IT 2351 Spring 2021 Assignment 2 Part 2 H
--- Below is the script I ran, and then I'm running a SELECT statement to show the data is present
-
--- INSERT INTO customers
--- VALUES (26,"Matt","Mahoney","123 Any Street","Cleveland","OH",44145,2168675309,"NULL")
-
-SELECT * FROM customers;
